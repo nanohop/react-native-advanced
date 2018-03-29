0a61dfc9027256c7172b6d5e3a9f23c13ad62715
Chris Achard <chrisachard@gmail.com>
2018-03-29 16:32:34 -0400

    Animates the deletion of todo items with the Animated feature of react native

--- TodoListAdvanced/src/screens/TodoList/TodoItem.js
+++ TodoListAdvanced/src/screens/TodoList/TodoItem.js
@@ -4,7 +4,8 @@ import {
   Text,
   View,
   TouchableOpacity,
-  LayoutAnimation
+  LayoutAnimation,
+  Animated
 } from 'react-native';
 
 import { Icon } from 'native-base'
@@ -17,6 +18,8 @@ export default class TodoItem extends Component {
   }
 
   componentDidMount() {
+    this.visible = new Animated.Value(100)
+
     requestAnimationFrame(() => {
       LayoutAnimation.spring()
       this.setState({
@@ -28,9 +31,17 @@ export default class TodoItem extends Component {
 
   componentDidUpdate(prevProps) {
     if(this.props.item.deleted && !prevProps.item.deleted) {
-      setTimeout(() => {
+      
+      Animated.timing(
+        this.visible,
+        {
+          toValue: 0,
+          duration: 250
+        }
+      ).start(e => {
         this.props.deleteTodoAPI(this.props.item.id)
-      }, 500)
+      })
+
     }
   }
 
@@ -46,30 +57,53 @@ export default class TodoItem extends Component {
   }
 
   render() {
+    const opacity = this.visible && this.visible.interpolate({
+      inputRange: [0, 100],
+      outputRange: [0, 1],
+    })
+
+    const left = this.visible && this.visible.interpolate({
+      inputRange: [0, 100],
+      outputRange: [-200, 0],
+    })
+
+    const height = this.visible && this.visible.interpolate({
+      inputRange: [0, 100],
+      outputRange: [0, 60],
+    })
+
     const item = this.props.item
     return (
-      <TouchableOpacity 
-        onPress={this.toggleTodo}
-        style={[
-            styles.itemButton,
-            {
-              height: this.state.height,
-              marginRight: this.state.padding,
-              marginLeft: this.state.padding
-            }
-          ]}
+      <Animated.View
+        style={{
+          opacity,
+          left,
+          height
+        }}
       >
-        <Icon name={item.completed ? 'checkmark-circle' : 'radio-button-off'} />
-        <Text style={[styles.item, {
-          opacity: (item.completed ? 0.5 : 1.0),
-          textDecorationLine: (item.completed ? 'line-through' : 'none')
-        }]}>
-          {item.task}
-        </Text>
-        <TouchableOpacity onPress={this.deleteTodo}>
-          <Icon name="trash" style={{ color: 'red', paddingRight: 10 }} />
+        <TouchableOpacity 
+          onPress={this.toggleTodo}
+          style={[
+              styles.itemButton,
+              {
+                height: this.state.height,
+                marginRight: this.state.padding,
+                marginLeft: this.state.padding
+              }
+            ]}
+        >
+          <Icon name={item.completed ? 'checkmark-circle' : 'radio-button-off'} />
+          <Text style={[styles.item, {
+            opacity: (item.completed ? 0.5 : 1.0),
+            textDecorationLine: (item.completed ? 'line-through' : 'none')
+          }]}>
+            {item.task}
+          </Text>
+          <TouchableOpacity onPress={this.deleteTodo}>
+            <Icon name="trash" style={{ color: 'red', paddingRight: 10 }} />
+          </TouchableOpacity>
         </TouchableOpacity>
-      </TouchableOpacity>
+      </Animated.View>
     )
   }
 
